d47b1c2fb65d6ae2bdce3ee867e5cd215b671c3e
Oded Viner <oviner@redhat.com>
2021-12-04 15:30:24 +0200

    fix code

--- tests/test_tier1.py
+++ tests/test_tier1.py
@@ -1,9 +1,8 @@
 import logging
 import pytest
 import os
-from framework.marks import ui_mark, tier1
+from framework.marks import tier1
 
-from ui.version_ui import VersionUI
 
 logger = logging.getLogger(__name__)
 
@@ -13,28 +12,10 @@ class TestUserInterface1(object):
     Test User Interface Validation
 
     """
-    @ui_mark
-    @pytest.mark.parametrize("version_id", [os.getenv("version_web")])
-    def test_ui(self, setup_ui, version_id):
-        """
-        Validate User Interface
-
-        Args:
-            setup_ui: login function on conftest file
-
-        """
-        version_ui = VersionUI(setup_ui)
-        version_ui.check_version(version=version_id)
-
     @tier1
     @pytest.mark.parametrize("version_id", [os.getenv("version_web")])
-    def test_tier1(self, setup_ui, version_id):
+    def test_tier1(self, version_id):
         """
         Validate User Interface
-
-        Args:
-            setup_ui: login function on conftest file
-
         """
-        version_ui = VersionUI(setup_ui)
-        version_ui.check_version(version=version_id)
+        logger.info(version_id)
--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,7 +1,7 @@
 import logging
 import pytest
 import os
-from framework.marks import ui_mark, tier1
+from framework.marks import ui_mark
 
 from ui.version_ui import VersionUI
 
@@ -16,19 +16,6 @@ class TestUserInterface(object):
     @ui_mark
     @pytest.mark.parametrize("version_id", [os.getenv("version_web")])
     def test_version_ui(self, setup_ui, version_id):
-        """
-        Validate User Interface
-
-        Args:
-            setup_ui: login function on conftest file
-
-        """
-        version_ui = VersionUI(setup_ui)
-        version_ui.check_version(version=version_id)
-
-    @tier1
-    @pytest.mark.parametrize("version_id", [os.getenv("version_web")])
-    def test_version_tier1(self, setup_ui, version_id):
         """
         Validate User Interface
 
